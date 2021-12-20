0f360dae11e336c7baa17a4aee94917e6d7b8033
LuBoNi <luputbogdan87@yahoo.com>
2021-12-20 12:28:16 +0200

    problema 14

--- Program.cs
+++ Program.cs
@@ -405,6 +405,28 @@ namespace setul3
             Console.WriteLine("Interschimbati elementele unui vector in asa fel incat la final toate valorile egale cu zero sa ajunga la sfarsit. (nu se vor folosi vectori suplimentari - operatia se va realiza inplace cu un algoritm eficient - se va face o singura parcugere a vectorului).");
             Console.Write("n: ");
             int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n + 1];
+            Console.WriteLine("Citeste vectorul:");
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            int nr_zero = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (v[i] == 0)
+                {
+                    nr_zero++;
+                    for (int j = i; j < n - 1; j++)
+                        v[j] = v[j + 1];
+                }
+            }
+            for (int i = n - nr_zero; i < n; i++)
+                v[i] = 0;
+                for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
         }
 
         private static void Problema15()
